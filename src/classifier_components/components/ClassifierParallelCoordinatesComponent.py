669f81df9b3c2df07ceff8ffdcaa8c229fccf483
Ethan Temple-Betts <psyet5@exmail.nottingham.ac.uk>
2023-03-01 16:31:57 +0000

    fix - Add string label to true class dimension
    Previously, the true class dimension was labeled with integer values,
    which were the result of a mapping from class -> int. Now the label shown
    on the axis are just the classes themselves.

--- src/classifier_components/components/ClassifierParallelCoordinatesComponent.py
+++ src/classifier_components/components/ClassifierParallelCoordinatesComponent.py
@@ -4,6 +4,7 @@ import plotly.express as px
 import pandas as pd
 from dash import dcc
 import numpy as np
+import plotly.graph_objects as go
 
 """
 AUTHOR: Ethan Temple-Betts
@@ -51,41 +52,26 @@ class ClassifierParallelCoordinatesComponent(ClassifierComponent):
         pred = pd.DataFrame() 
         pred[classifier] = predictions
 
-        # Generate an array to contain all unique
-        # classifications
-        classes = (np.unique(pred[classifier])).tolist()
-
-        # This dictionary will store the mappings from class
-        # names to ints
-        mapping = {}
-        count = 0
-
-        # Val is an array of ints [0..n] where n is the 
-        # number of unique classes
-        val = list(range(len(classes)))
-
-        # Populate the dictionay with mappings from classes to
-        # ints
-        for c in classes:
-            mapping[c] = val[count]
-            count+=1
-
-        # Convert the class names to ints in the
-        # both dataframe. This allows the plot to work with
-        # classification data when the classes are strings
-        pred[classifier] = pred[classifier].map(mapping)
-        yTestDF[classifier] = yTestDF[classifier].map(mapping)
-
         # Add a column to the xTest dataframe which contains
         # the true classifications of the data.
         # .values is needed here to prevent indexing issues
         # between the 2 dataframes
         xTest["True Class"] = yTestDF[classifier].values
 
-        # dims contains all the dimensions used for the plot
-        # i.e all the columns in xTest including
-        # the true classifications
-        dims = xTest.columns
+        # Create an array containing the all unique classes
+        classes = (np.unique(pred[classifier])).tolist()
+
+        # The mapping dictionary will contain mappings
+        # from class -> int so that the classification
+        # can be used to colour the lines
+        mapping = {}
+        count = 0
+        val = list(range(len(classes)))
+        for c in classes:
+            mapping[c] = val[count]
+            count+=1
+        
+        pred[classifier] = pred[classifier].map(mapping)
 
         # Add the predicted classes as a column in xTest.
         # (This won't show in the plot as a dimension but is used
@@ -93,16 +79,58 @@ class ClassifierParallelCoordinatesComponent(ClassifierComponent):
         # prevent indexing issues
         xTest[classifier] = pred[classifier].values
 
+        # Map the true classfication -> int and store these
+        # values in a 'dummy' column
+        dfg = pd.DataFrame({"True Class":xTest["True Class"].unique()})
+        dfg['dummy'] = dfg.index
+        xTest = pd.merge(xTest, dfg, on = "True Class", how='left')
 
-        # Create the plot using the xTest dataframe, and colour
-        # based on predicted class
-        fig = px.parallel_coordinates(xTest, color=classifier,
-                                    dimensions=dims,
-                                    color_continuous_scale=px.colors.diverging.Spectral,
-                                    color_continuous_midpoint=2)
+        # An aray to store the dimensions (features)
+        # used for the plot
+        dimensions = []
+        features = xTest.columns.values.tolist()
 
+        # True Class could still contain str values
+        # so needs to be removed
+        features.remove("True Class")
+
+        # We don't want the dummy values to be plotted
+        # so they are removed
+        features.remove('dummy')
+
+        # This feature is used to assign colour
+        # and should not be a dimension so it is removed
+        features.remove(classifier)
+
+        # Iterate through the remaining features and create
+        # dimensions for each of them
+        for i in features:
+            dim = dict(range=[xTest[i].min(),xTest[i].max()],
+                    label=i,
+                    values=xTest[i])
+            
+            dimensions.append(dim)
+
+        # Create the 'True Class' dimension and use the
+        # dummy values to plot data but the class label
+        # as the axis annotations
+        dim = dict(range=[0, xTest['dummy'].max()],
+                tickvals = dfg['dummy'], ticktext = dfg["True Class"],
+                label="True Class", values=xTest['dummy'])
+        dimensions.append(dim)
+
+        # Create the plot and use the predicted class to colour
+        # the lines
+        fig = go.Figure(data=go.Parcoords(
+            line = dict(color = xTest[classifier]),
+            dimensions=dimensions))
+        
         # Customise the colours of the plot
-        fig.update_layout(paper_bgcolor="#232323", font_color = "#f5f5f5", plot_bgcolor="#232323")
+        fig.update_layout(
+            paper_bgcolor="#232323",
+            font_color = "#f5f5f5",
+            plot_bgcolor="#232323")
+        
         self.componentTitle = "Parallel Coordinates"
 
         # Update the componentChildren property to be a html div
