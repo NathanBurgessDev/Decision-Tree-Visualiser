5092ebce5b705f95928dc2726f840a3d17d57509
Ethan Temple-Betts <psyet5@exmail.nottingham.ac.uk>
2023-03-05 14:19:58 +0000

    Doc - Improve Readability Of ParallelCoordinatesComponent.py
    The __init__ function has been decomposed into multiple functions to
    improve code readability. Comments have been shortened and made more
    descriptive.

--- src/classifier_components/components/ClassifierParallelCoordinatesComponent.py
+++ src/classifier_components/components/ClassifierParallelCoordinatesComponent.py
@@ -13,7 +13,7 @@ PREVIOUS MAINTAINER: Ethan Temple-Betts
 DATE LAST MODIFIED: 04/03/2023
 
 This class will create a parallel coordinates plot
-using the TESTING data for the model. The colour of the lines
+using the testing data for the model. The colour of the lines
 represents the predeicted classification.
 
 Inputs
@@ -29,99 +29,52 @@ dict modelInfo :    {"modelData" : model,
 class ClassifierParallelCoordinatesComponent(ClassifierComponent):
 
     def __init__(self, modelInfo):
-        # Extract x and y test data from modelinfo
+
+        # Extract xTest data, yTest data and the model
         xTest = modelInfo["testingData"][0]
         yTest = modelInfo["testingData"][1]
-
-        # Extract the MLM
         model = modelInfo['modelData']
 
-        # Use the model to make class predictions for the
-        # testing dataset
+        # Predict the classes of xTest data and store result in
+        # pred dataframe
         predictions = model.predict(xTest)
-
-        # Convert the yTest data to a dataframe
-        # which will contain the correct classifications
         yTestDF = yTest.to_frame()
-
-        # The classifier variable is the feature name
-        # that the user selected as the classifier
         classifier = yTestDF.columns[0]
-
-        # Create a dataframe to hold the class predictions
         pred = pd.DataFrame() 
         pred[classifier] = predictions
 
-        # Add a column to the xTest dataframe which contains
-        # the true classifications of the data.
-        # .values is needed here to prevent indexing issues
-        # between the 2 dataframes
+        # Add the true class and predicted class data
+        # to the xTest data frame
         xTest["True Class"] = yTestDF[classifier].values
-
-        # Create an array containing the all unique classes
         classes = (np.unique(pred[classifier])).tolist()
-
-        # The mapping dictionary will contain mappings
-        # from class -> int so that the classification
-        # can be used to colour the lines
-        mapping = {}
-        count = 0
-        val = list(range(len(classes)))
-        for c in classes:
-            mapping[c] = val[count]
-            count+=1
-        
+        mapping = self.createClassMap(classes)
         pred[classifier] = pred[classifier].map(mapping)
-
-        # Add the predicted classes as a column in xTest.
-        # (This won't show in the plot as a dimension but is used
-        # to colour the lines). .values is again important to
-        # prevent indexing issues
         xTest[classifier] = pred[classifier].values
 
-        # Map the true classfication -> int and store these
-        # values in a 'dummy' column
+        # Map the true classes -> ints and store these
+        # values in a 'dummy' column in xTest
         dfg = pd.DataFrame({"True Class":xTest["True Class"].unique()})
         dfg['dummy'] = dfg.index
         xTest = pd.merge(xTest, dfg, on = "True Class", how='left')
 
-        # An aray to store the dimensions (features)
-        # used for the plot
-        dimensions = []
+        # Create an array of feature names and remove
+        # the true class, dummy and predicted columns
         features = xTest.columns.values.tolist()
-
-        # True Class could still contain str values
-        # so needs to be removed
         features.remove("True Class")
-
-        # We don't want the dummy values to be plotted
-        # yet so they are removed
         features.remove('dummy')
-
-        # This feature is used to assign colour
-        # and should not be a dimension so it is removed
         features.remove(classifier)
 
-        # Iterate through the remaining features and create
-        # dimensions for each of them
-        for i in features:
-            dim = dict(range=[xTest[i].min(),xTest[i].max()],
-                    label=i,
-                    values=xTest[i],
-                    visible=False)
-            
-            dimensions.append(dim)
+        # Create dimensions for the remaining features
+        dimensions = self.createPlotDimensions(features, xTest)
 
-        # Create the 'True Class' dimension and use the
-        # dummy values to plot data but the class label
-        # as the axis annotations
+        # Create the 'True Class' dimension and plot data using
+        # dummy values and label the axis with the true class
         dim = dict(range=[0, xTest['dummy'].max()],
                 tickvals = dfg['dummy'], ticktext = dfg["True Class"],
                 label="True Class", values=xTest['dummy'])
         dimensions.append(dim)
 
-        # Create the plot and use the predicted class to colour
-        # the lines
+        # Create the plot and color using predicted class
         fig = go.Figure(data=go.Parcoords(
             line = dict(color = xTest[classifier],
                         colorscale = 'spectral'),
@@ -136,7 +89,7 @@ class ClassifierParallelCoordinatesComponent(ClassifierComponent):
         self.componentTitle = "Parallel Coordinates"
 
         # Update the componentChildren property to be a html div
-        # containing a seperate div for dimension control;
+        # containing a seperate div for dimension controls;
         # div[('-' button), (feature dropdown), ('+' button)]
         # and below this the parallel coordinates plot
         self.componentChildren = html.Div(
@@ -161,6 +114,60 @@ class ClassifierParallelCoordinatesComponent(ClassifierComponent):
             dcc.Graph(
             figure = fig,
             id="pc_plot")])
+        
+    
+    ''' 
+    AUTHOR: Ethan Temple-Betts
+    PREVIOUS MAINTAINER: Ethan Temple-Betts
+
+    Create a dictionary, which maps class names -> ints
+
+    Inputs:
+    list classes[str or int] : a list of unique class names
+
+    Returns:
+    dict mapping : A dictionary mapping class names -> int
+    
+    '''
+    def createClassMap(self, classes):
+        mapping = {}
+        count = 0
+        val = list(range(len(classes)))
+        for c in classes:
+            mapping[c] = val[count]
+            count+=1
+
+        return mapping
+    
+
+    ''' 
+    AUTHOR: Ethan Temple-Betts
+    PREVIOUS MAINTAINER: Ethan Temple-Betts
+
+    Create a list, which contains valid paracoord dimensions
+
+    Inputs:
+    list[str] features : a list of feature names in data to be
+    made into dimensions
+
+    dataframe data : A dataframe containing the data to be
+    plotted 
+
+    Returns:
+    list[dict] dimensions : Valid paracoord dimensions
+    '''
+    def createPlotDimensions(self, features, data):
+        dimensions = []
+        for i in features:
+            dim = dict(range=[data[i].min(),data[i].max()],
+                    label=i,
+                    values=data[i],
+                    visible=False)
+            
+            dimensions.append(dim)
+
+        return dimensions
+
 
 
     
