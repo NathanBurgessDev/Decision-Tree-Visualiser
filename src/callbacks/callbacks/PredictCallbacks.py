7be8f86e74e65132b697787a235df0f74293c936
Dominic Cripps <psydc8@nottingham.ac.uk>
2023-03-21 15:15:49 +0000

    feat : Add Tree Highlight Path For User Input
    This commit will add the functionality to get the specific path a user
    inputted value will take, it will then draw this path onto a figure.
    Next step will be to add this trace onto the original tree plot.

--- src/callbacks/callbacks/PredictCallbacks.py
+++ src/callbacks/callbacks/PredictCallbacks.py
@@ -4,6 +4,8 @@ import dash
 from UserSession import UserSession
 import pandas as pd
 import numpy as np
+from utils.TreeUtil import TreeUtil
+from igraph import Graph, EdgeSeq
 
 import plotly.graph_objects as go
 from dash import dcc
@@ -13,8 +15,8 @@ def get_callbacks(app):
     """
     AUTHOR: Dominic Cripps
     DATE CREATED: 23/02/2023
-    PREVIOUS MAINTAINER: Daniel Ferring
-    DATE LAST MODIFIED: 19/03/2023
+    PREVIOUS MAINTAINER: Dominic Cripps
+    DATE LAST MODIFIED: 21/03/2023
 
     Callback is triggered when the button 'predict-button' is pressed.
 
@@ -30,15 +32,92 @@ def get_callbacks(app):
          Output(component_id="decision-boundary-component", component_property="children")],
         [Input("predict-button", component_property="n_clicks"),
             State(dict(name="prediction-features", idx=ALL), "value"),
-            State("decision-boundary-component", component_property = "children")]
+            State("decision-boundary-component", component_property = "children"),
+            Input("trained-models", component_property="value")]
     )
-    def predictInput(clicks, features, boundaryData):
+    def predictInput(clicks, features, boundaryData, modelFilename):
         
         if "predict-button" == ctx.triggered_id:
             if not None in features:
+                modelInfo = UserSession().instance.modelInformation[modelFilename]
+
                 df = pd.DataFrame(data = np.array([features]), 
                     columns = UserSession().instance.selectedModel.feature_names_in_)
 
+                #tree highlight behaviour 
+                if(modelInfo["classifierType"] == "DecisionTreeClassifier"):
+                    tree = modelInfo["modelData"].tree_
+                    featureNames = modelInfo["modelData"].feature_names_in_
+                    x = [0.0]
+                    y = [10.0]
+                    path = []
+                    currX = 0
+                    depth = 0
+                    currNode = 0
+                    while(tree.children_left[currNode] != -1 and tree.children_right[currNode] != -1):
+                        threshold = tree.threshold[currNode]
+                        previousNode = currNode
+                        if(depth > 0):
+                            x.append(x[len(x) - 2])
+                            y.append(y[len(y) - 2])
+
+                        if(float(df[featureNames[tree.feature[currNode]]].iloc[0]) <= float(threshold)):
+                            currX -= 0.5
+                            x.extend([currX, None])
+                            currNode += 1
+                        else:
+                            currX += 0.5
+                            x.extend([currX, None])
+                            currNode += 2
+
+                        path.append((previousNode, currNode))
+                        depth += 1
+                        y.extend([10.0 - depth, None])
+
+                    treeUtil = TreeUtil()
+                    treeUtil.generateDecisionTree(modelInfo["modelData"], modelInfo["modelData"], modelInfo["modelData"].tree_)
+
+                    print(path)
+                    graphComp = Graph(directed = "T")
+                    vertices = list(range(0, modelInfo["modelData"].tree_.node_count))
+                    graphComp.add_vertices(treeUtil.getVerticies())
+                    graphComp.add_edges(treeUtil.getEdges())
+                    print(graphComp)
+
+                    lay = graphComp.layout('rt')
+                    v_label = list(map(str, range(modelInfo["modelData"].tree_.node_count)))
+                    position = {k: lay[k] for k in range(modelInfo["modelData"].tree_.node_count)}
+                    Y = [lay[k][1] for k in range(modelInfo["modelData"].tree_.node_count)]
+                    M = max(Y)
+
+                    es = EdgeSeq(graphComp)
+                    E = path
+
+                    L = len(position)
+                    Xn = [position[k][0] for k in range(L)]
+                    Yn = [2*M-position[k][1] for k in range(L)]
+                    Xe = []
+                    Ye = []
+                    for edge in E:
+                        Xe+=[position[edge[0]][0],position[edge[1]][0], None]
+                        Ye+=[2*M-position[edge[0]][1],2*M-position[edge[1]][1], None]
+
+                    print(Xe)
+                    print(Ye)
+
+
+                    fig = go.Figure()
+                    fig.add_trace(go.Scatter(x=Xe,
+                        y=Ye,
+                        mode='lines',
+                        line=dict(color='rgb(210,210,210)', width=2),
+                        hoverinfo='none'
+                        ))    
+                    fig.show()
+
+
+                
+                #boundary plot point behaviour
                 classification = UserSession().instance.selectedModel.predict(df)
                 numFeatures = len(features)
 
--- src/classifier_components/components/ClassifierTreeComponent.py
+++ src/classifier_components/components/ClassifierTreeComponent.py
@@ -1,5 +1,6 @@
 from classifier_components.ClassifierComponent import ClassifierComponent
 from dash import html
+from UserSession import UserSession
 from utils.TreeUtil import TreeUtil
 
 """
@@ -21,7 +22,7 @@ class ClassifierTreeComponent(ClassifierComponent):
         # The model is parsed to 'TreeUtil.generateDecisionTree', this will 
         # return a 'dcc.Graph' object containing the tree
         self.tree = treeUtil.generateDecisionTree(modelInfo["modelData"], modelInfo["modelData"], modelInfo["modelData"].tree_)
-
+        UserSession().instance.selectedTree = self.tree
         self.componentTitle = "Model Decision Tree"
         # Set component layout property to be a div containing the tree graph
         # Important : className of this div must be "classifierComponent" to format correctly
