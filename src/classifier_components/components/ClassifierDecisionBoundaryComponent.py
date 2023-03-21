25d5df53e05b2fc0ecc61b36f164e4cdd14ac907
Daniel Ferring <psydf3@nottingham.ac.uk>
2023-03-21 14:14:49 +0000

    feat: add example tooltip to decison boundary component
    This commit adds an example tooltip for the decision boundary visualisation.
    It should be noted that the css styling of the can be improved and the
    description is an intial draft; this is only meant to serve as a rough
    template/proof of concept for future tooltips

--- src/classifier_components/components/ClassifierDecisionBoundaryComponent.py
+++ src/classifier_components/components/ClassifierDecisionBoundaryComponent.py
@@ -1,15 +1,13 @@
 from classifier_components.ClassifierComponent import ClassifierComponent
 from dash import html
 from utils.DecisionBoundaryUtil import DecisionBoundaryUtil
-
-
-
+import dash_bootstrap_components as dbc
 
 """
 AUTHOR: Daniel Ferring
 DATE CREATED: 19/02/2023
 PREVIOUS MAINTAINER: Daniel Ferring
-DATE LAST MODIFIED: 19/03/2023
+DATE LAST MODIFIED: 21/03/2023
 
 Child of 'ClassifierComponent', this class defines an
 appropriate 'componentLayout' to represent decision boundaries
@@ -24,12 +22,30 @@ class ClassifierDecisionBoundaryComponent(ClassifierComponent):
 
     def __init__(self, modelInfo):
         
+        title = 'Decision Boundary'
+        description = 'This visualisation represents the decisions made by the tree. '\
+        'The axes are the features used to train the model and the data instances are plotted within the feature space as markers. '\
+        'The colours of the instances represent their true class (taken from the data set). The colour of the section that they are plotted in '\
+        'represents the class that the model would predict them to be. It should be noted that the colour of the markers is slightly darker than '\
+        'that of their predicted class, this is done to make them more distinguishable from the background.'
+        
         BoundaryUtil = DecisionBoundaryUtil()
 
         if(len(modelInfo["modelData"].feature_names_in_) > 2):
             self.boundary = html.P("Higher Dimensions are not supported for this visualisation")
         else:
-            self.boundary = BoundaryUtil.generateDecisionBoundary(modelInfo)
+            self.boundary = [html.Div(id = "decision-boundary", children = BoundaryUtil.generateDecisionBoundary(modelInfo)),
+                            dbc.Tooltip(
+                                children = [
+                                    html.Div(children = [
+                                        html.H1(title, style = {'color' : 'blue'}),
+                                        html.P(description, style = {'color' : 'black'})
+                                    ],
+                                style={'background-color' : 'GhostWhite', 'width' : '600px'})],
+                                target = "decision-boundary",
+                                placement = "bottom",
+                )
+            ]
 
         #Sets the values for the component to be displayed within the app
         self.componentTitle = "Decision Boundary Visualisation"
