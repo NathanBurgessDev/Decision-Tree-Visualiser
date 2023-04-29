9245ffabd079b33adfe124fd1345c82ca437ee63
Alfred Greenwood <psyag7@exmail.nottingham.ac.uk>
2023-04-29 15:16:26 +0100

    feat: Add SVM decision boundary initial implementation

--- src/classifier_components/components/ClassifierSVMDecisionBoundaryComponent.py
+++ src/classifier_components/components/ClassifierSVMDecisionBoundaryComponent.py
@@ -8,6 +8,7 @@ from dash import dcc
 from dash import html
 from plotly.graph_objs import *
 from utils.DecisionBoundaryUtil import DecisionBoundaryUtil
+from sklearn.inspection import DecisionBoundaryDisplay as DBD
 import numpy as np
 
 """
@@ -15,7 +16,7 @@ import numpy as np
 AUTHOR: Alfred Greenwood
 DATE CREATED: 03/03/2023
 PREVIOUS MAINTAINER: Alfred Greenwood
-DATE LAST MODIFIED: 07/04/2023
+DATE LAST MODIFIED: 29/04/2023
 
 Child of 'ClassifierComponent', this class defines an
 appropriate 'componentLayout' to represent user input.
@@ -39,77 +40,73 @@ Inputs:
 class ClassifierSVMDecisionBoundaryComponent(ClassifierComponent):
     def __init__(self, modelInfo):
         self.svc = modelInfo["modelData"]
-        
                 
         self.componentTitle = "SVM Decision boundary"
         
-        if(len(modelInfo["modelData"].feature_names_in_) > 3):
+        if(len(modelInfo["modelData"].feature_names_in_) > 2):
             self.graph = html.P("Higher Dimensions are not yet implemented")
         elif(len(modelInfo["modelData"].feature_names_in_) <= 1):
             self.graph = html.P("More than 1 dimension must be implemented")
-        elif(len(modelInfo["modelData"].feature_names_in_) == 3):
-            features = modelInfo["trainingData"][0]
-
-            # Need a better name for this variable
-            classifiers = modelInfo["trainingData"][1]
-
-            dataKeys = features.keys()
-            
-            self.fig = go.FigureWidget()
-            z = lambda x,y: (-self.svc.intercept_[0]-self.svc.coef_[0][0]*x-self.svc.coef_[0][1]*y) / self.svc.coef_[0][2]
-
-            #tmp = np.linspace(-2,2,51)
-            #x,y = np.meshgrid(tmp,tmp)
-
-            for X in classifiers.unique():
-                for Y in classifiers.unique():
-                    if X != Y:
-                        xm, xM = X[:,0].min(), X[:, 0].max()
-                        ym, yM = X[:,1].min(), X[:, 1].max()
-                        x = np.linspace(xm, xM, 10)
-                        y = np.linspace(ym, yM, 10)
-                        x, y =np.meshgrid(x, y)
-                        self.fig.add_surface(x=x, y=y, z=z(x,y), colorscale='Greys', showscale=False, opacity=0.9)
-
-            self.fig.add_scatter3d(x=dataKeys[0], y=dataKeys[1], z=dataKeys[2], mode='markers', marker=dict(color=classifiers), showlegend=True)
-            
-            self.fig.update_layout(
-                # Update axis names
-                autosize=True, 
-                margin={'t': 50,'l':10,'b':5,'r':30},
-
-                # Customise the colours of the plot
-                paper_bgcolor="#232323", 
-                font_color = "#f5f5f5", 
-                plot_bgcolor="#232323")
-
-
-            self.graph = dcc.Graph(figure = self.fig)
-
         else:
             features = modelInfo["trainingData"][0]
 
             # Need a better name for this variable
             classifiers = modelInfo["trainingData"][1]
-
+            
             dataKeys = features.keys()
+
+            #hyperPlanes = []
+
+            xx = np.linspace(modelInfo["trainingData"][0][dataKeys[0]].min(), modelInfo["trainingData"][0][dataKeys[0]].max())
+            hyperPlanes = pd.DataFrame(columns=["Classifier","yy", "upper", "lower", "xx"])
+            hyperPlane = []
             
-            self.fig = go.FigureWidget()
+            if(self.svc.kernel == "linear"):
+                for i in range(0, len(self.svc.coef_)):
+                    w = self.svc.coef_[i]
+                    a = -w[0] / w[1]                    
+                    yy = a * xx - (self.svc.intercept_[i]) / w[1]
+
+                    b = self.svc.support_vectors_[0]
+                    yy_down = a * xx + (b[1] - a * b[0])
+                    b = self.svc.support_vectors_[-1]
+                    yy_up = a * xx + (b[1] - a * b[0])
+                    
+                    hyperPlane.append(pd.DataFrame(({"Classifier": i, "yy": yy, "upper": yy_up, "lower": yy_down, "xx": xx})))
                         
-            self.fig.add_scatter(x=modelInfo["trainingData"][0][dataKeys[0]], y=modelInfo["trainingData"][0][dataKeys[1]], mode='markers', marker=dict(color=classifiers), showlegend=True)
-            
-            self.fig.update_layout(
-                # Update axis names
-                autosize=True, 
-                margin={'t': 50,'l':10,'b':5,'r':30},
+                hyperPlanes = pd.concat(hyperPlane)
 
-                # Customise the colours of the plot
-                paper_bgcolor="#232323", 
-                font_color = "#f5f5f5", 
-                plot_bgcolor="#232323")
+                scatterData = px.scatter(x=modelInfo["trainingData"][0][dataKeys[0]], y=modelInfo["trainingData"][0][dataKeys[1]], color=classifiers)
+                dat = scatterData.data
+
+                line = px.line(hyperPlanes, x=hyperPlanes["xx"], y=hyperPlanes["yy"], color=hyperPlanes["Classifier"])
+
+                dat += line.data
+                self.fig=go.Figure(data=dat)
+                
+                minY = float(modelInfo["trainingData"][0][dataKeys[1]].min())
+                maxY = float(modelInfo["trainingData"][0][dataKeys[1]].max())
+                delta = maxY-minY
+                minY -= delta*0.05
+                maxY += delta*0.05
+
+                self.fig.update_layout(
+                    # Configure max values on axis to be 5% of the largest and smallest values on the y axis
+                    # to prevent near-vertical Hyperplanes taking up the entire y axis
+                    yaxis_range=[minY, maxY],
+
+                    # Update axis names
+                    margin={'t': 50,'l':10,'b':5,'r':30},
+
+                    # Customise the colours of the plot
+                    paper_bgcolor="#232323", 
+                    font_color = "#f5f5f5", 
+                    plot_bgcolor="#232323")
 
 
-            self.graph = dcc.Graph(figure = self.fig)
+                self.graph = dcc.Graph(figure = self.fig)
+            else:
+                self.graph = html.P("SVM decision boundaries can only be visualised currently for linear SVM kernels")
         
         
         self.componentChildren = html.Div(self.graph)
